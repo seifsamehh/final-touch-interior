47803fa41e4f5ac198d6adaca5c91cfb0040d65d
seifsamehh <seifsameh1432@gmail.com>
2023-07-01 10:50:20 +0300

    Final Touch Interior

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
 const queryClient = new QueryClient();
 import { ParallaxProvider } from "react-scroll-parallax";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App.jsx";
 import "./index.css";
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
           <App />
+          <Analytics />
         </QueryClientProvider>
       </BrowserRouter>
     </ParallaxProvider>
